1640cb774f8b3add35e8b19ab9e143aaa68380a6
fieldsb2 <112001235+fieldsb2@users.noreply.github.com>
2023-09-28 14:23:24 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿
+
+
+using System.Net.Http;
+
+bool running = true;
+string MenuSelector = "";
+HttpClient httpClient = new HttpClient();
+string UserFruit = "";
+List<string> FruitList = new List<string>();
+
+Console.WriteLine("Welcome to the fruit market. Select what you would like to do by entering a number: ");
+
+while (running)
+{
+    Console.WriteLine(
+    "\n1. Type in a fruit to learn more about it before you purchase it." +
+    "\n2. Type in a fruit to add it to your cart." +
+    "\n3. View Cart." +
+    "\n4. End the program.");
+
+    Console.Write("\n");
+    MenuSelector = Console.ReadLine();
+    if (MenuSelector == "1")
+    {
+        Console.WriteLine("\nEnter the fruit you want to research.");
+        Console.Write("\n");
+        UserFruit = Console.ReadLine();
+        string baseURL = "https://fruityvice.com";
+        string apiURL = $"{baseURL}/api/fruit/{UserFruit}";
+        HttpResponseMessage httpResponse = await httpClient.GetAsync(apiURL);
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            string apiOutput = await httpResponse.Content.ReadAsStringAsync();
+            Console.WriteLine("\n" + apiOutput);
+
+        }
+        else
+        {
+            Console.WriteLine("Fruit not found within system. Please try again");
+        }
+        
+
+        running = true;
+    }
+    else if (MenuSelector == "2")
+    {
+        Console.WriteLine("\nEnter the fruit you would like to add to cart.");
+        Console.Write ("\n");
+        UserFruit = Console.ReadLine();
+        FruitList.Add(UserFruit);
+        Console.WriteLine("\n" + UserFruit + " added to cart.");
+
+        running = true;
+    }
+    else if (MenuSelector == "3")
+    {
+        Console.WriteLine("\n----------------------------------");
+        foreach (string fruit in FruitList)
+        {
+            Console.WriteLine(fruit + "\n");
+        }
+        Console.WriteLine("----------------------------------");
+        running = true;
+    }
+    else if (MenuSelector == "4")
+    {
+        Console.WriteLine("Thank you for using Fruit Market. Here is what is in your cart: \n");
+
+        Console.WriteLine("\n----------------------------------");
+        foreach (string fruit in FruitList)
+        {
+            Console.WriteLine(fruit + "\n");
+        }
+        Console.WriteLine("----------------------------------");
+
+        Console.WriteLine("Have a good day!");
+        running = false;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number from the list.");
+        running = true;
+    }
+
+}
