4d3702ceab393d5a89a4fcd75413e2fb058a8ae0
fieldsb2 <112001235+fieldsb2@users.noreply.github.com>
2023-09-28 20:12:05 -0400

    Add files via upload

---
+++ Fruit.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace FruitMarket
+{
+    public class Fruit
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("family")]
+        public string Family { get; set; }
+        [JsonPropertyName("genus")]
+        public string Genus { get; set; }
+        [JsonPropertyName("order")]
+        public string Order { get; set; }
+        [JsonPropertyName("nutritions")]
+        public Dictionary<string, float> Nutrition { get; set; } = new Dictionary<string, float>();
+
+    }
+}
